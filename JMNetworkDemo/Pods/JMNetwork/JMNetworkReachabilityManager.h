37a78b9c3fca755e8ae81c31520cd03693d75493
James.xiao <1007785739@qq.com>
2016-11-01 19:59:21 +0800

    添加网络监控功能

---
+++ JMNetworkDemo/Pods/JMNetwork/JMNetworkReachabilityManager.h
@@ -0,0 +1,13 @@
+//
+//  JMNetworkReachabilityManager.h
+//  Pods
+//
+//  Created by James.xiao on 2016/11/1.
+//
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface JMNetworkReachabilityManager : AFNetworkReachabilityManager
+
+@end
